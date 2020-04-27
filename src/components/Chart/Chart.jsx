8ad044e91f9a5ab54d004853576f62613ab51543
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-27 17:45:49 -0400

    Fix typo in api call

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import {Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
 const Chart = () => {
-    const [dailyData, setDailyData] = useState({});
+    const [dailyData, setDailyData] = useState([]);
 
 
     useEffect(
@@ -17,21 +17,35 @@ const Chart = () => {
             fetchAPI();
     });
 
-    const LineChart = (
-        dailyData[0]
+    const lineChart = (
+        dailyData.length
         ?(
         <Line
 
         data={{
-            labels:'',
-            datasets:[{},{}]
+            labels: dailyData.map(({date}) => date),
+            datasets:[{
+                data:dailyData.map(({confirmed}) => confirmed),
+                label: 'Infected',
+                borderColor:'#3333ff',
+                fill: true,
+            },{
+                data: dailyData.map(({deaths}) => deaths),
+                label: 'Deaths',
+                borderColor:'red',
+                backgorundColor:'rgba(255, 0, 0, 0.5)',
+                fill: true,
+        
+            }],
         }}
         />) :null
     );
 
 
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 }
 
