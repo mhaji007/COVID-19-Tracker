edc9335028ea38913377d5e6b53bf87acb8c08dd
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-27 13:04:36 -0400

    Apply default styling to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
 import {Cards, Chart, CountryPicker} from './components';
-import Styles from './App.module.css';
+import styles from './App.module.css';
 
 class App extends Component {
     render() { 
         return (
-            <div>
+            <div className={styles.container}>
                 <Cards/>
                 <CountryPicker/>
                 <Chart/>
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CountryPicker = () => {
     return (
-        <h1>Chart</h1>
+        <h1>CountryPicker</h1>
     )
 }
 
